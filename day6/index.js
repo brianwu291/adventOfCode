a96dbb581661f7d46c00bedc924c2dcd7ee08733
BrianWu <brianwu291@gmail.com>
2021-12-07 10:45:33 +0800

    day 6 second one

--- day6/index.js
+++ day6/index.js
@@ -5,44 +5,37 @@ function parseData(rawDataString = '') {
   return rawDataString.split(',').map(dataStr => parseInt(dataStr, 10));
 }
 
-function produceNewDataOnOneDay(data = []) {
-  const result = [...data];
-  let shouldAddNewFishCount = 0;
-  for (let i = 0; i < result.length; i += 1) {
-    const curTimer = result[i];
-    if (curTimer === 0) {
-      result[i] = 6;
-      shouldAddNewFishCount += 1;
-    } else {
-      result[i] = curTimer - 1;
-    }
-  }
-
-  for (let j = 1; j <= shouldAddNewFishCount; j += 1) {
-    result.push(8);
-  }
-
-  return result;
-}
 
 function calFirstOne(data = []) {
-  let result = [...data];
+  const queue = [...Array(9)].map(() => 0);
 
-  for (let i = 1; i <= 80; i += 1) {
-    result = produceNewDataOnOneDay(result);
+  for (const fish of data) {
+    queue[fish] += 1;
   }
 
-  return result.length;
+  for (let i = 1; i <= 80; i += 1) {
+    const currentFishes = queue.shift();
+    queue.push(currentFishes);
+    queue[6] += currentFishes;
+  }
+
+  return queue.reduce((a, b) => a + b, 0);
 }
 
 function calSecondOne(data = []) {
-  let result = [...data];
+  const queue = [...Array(9)].map(() => 0);
 
-  for (let i = 1; i <= 256; i += 1) {
-    result = produceNewDataOnOneDay(result);
+  for (const fish of data) {
+    queue[fish] += 1;
   }
 
-  return result.length;
+  for (let i = 1; i <= 256; i += 1) {
+    const currentFishes = queue.shift();
+    queue.push(currentFishes);
+    queue[6] += currentFishes;
+  }
+
+  return queue.reduce((a, b) => a + b, 0);
 }
 
 fs.readFile(`${path.resolve()}/day6/input.txt`, 'utf8' , (err, data) => {
@@ -52,8 +45,8 @@ fs.readFile(`${path.resolve()}/day6/input.txt`, 'utf8' , (err, data) => {
   }
   const parsedData = parseData(data);
 
-  // const firstOneRes = calFirstOne(parsedData);
-  // const secondOneRes = calSecondOne(parsedData);
-  // console.log('firstOneRes', firstOneRes);
-  // console.log('secondOneRes', secondOneRes);
+  const firstOneRes = calFirstOne(parsedData);
+  const secondOneRes = calSecondOne(parsedData);
+  console.log('firstOneRes', firstOneRes);
+  console.log('secondOneRes', secondOneRes);
 });
--- index.js
+++ index.js
@@ -2,4 +2,4 @@
 // require('./day3');
 // require('./day4');
 // require('./day5');
-require('./day6');
+// require('./day6');
